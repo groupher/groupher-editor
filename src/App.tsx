af47e9e4c4df87d5878e5de074e9868f097f7026
mydearxym <mydearxym@qq.com>
2025-09-02 15:02:56 +0800

    chore: wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import type { Value } from "platejs";
 
-import { StrictMode } from "react";
-
-import { BoldPlugin, ItalicPlugin, UnderlinePlugin } from "@platejs/basic-nodes/react";
+import {
+  BlockquotePlugin,
+  H1Plugin,
+  H2Plugin,
+  H3Plugin,
+  BoldPlugin,
+  ItalicPlugin,
+  UnderlinePlugin,
+} from "@platejs/basic-nodes/react";
 import { Plate, usePlateEditor } from "platejs/react";
 
 import { Editor, EditorContainer } from "@/components/ui/editor";
+import { BlockquoteElement } from "@/components/ui/blockquote-node";
+import { H1Element, H2Element, H3Element } from "@/components/ui/heading-node";
+import { ToolbarButton } from "@/components/ui/toolbar"; // Generic toolbar button
+
 import { FixedToolbar } from "@/components/ui/fixed-toolbar";
 import { MarkToolbarButton } from "@/components/ui/mark-toolbar-button";
 
 const initialValue: Value = [
+  {
+    children: [{ text: "Title" }],
+    type: "h3",
+  },
+  {
+    children: [{ text: "This is a quote." }],
+    type: "blockquote",
+  },
   {
     type: "p",
     children: [
@@ -26,30 +44,41 @@ const initialValue: Value = [
 
 export default function App() {
   const editor = usePlateEditor({
-    plugins: [BoldPlugin, ItalicPlugin, UnderlinePlugin], // mark plugins
+    plugins: [
+      BoldPlugin,
+      ItalicPlugin,
+      UnderlinePlugin,
+      H1Plugin.withComponent(H1Element),
+      H2Plugin.withComponent(H2Element),
+      H3Plugin.withComponent(H3Element),
+      BlockquotePlugin.withComponent(BlockquoteElement),
+    ], // mark plugins
     value: initialValue, // initial content
   });
 
   return (
     <div className="m-5 debug">
-      <StrictMode>
-        <Plate editor={editor}>
-          <FixedToolbar className="justify-start rounded-t-lg">
-            <MarkToolbarButton nodeType="bold" tooltip="Bold (⌘+B)">
-              B
-            </MarkToolbarButton>
-            <MarkToolbarButton nodeType="italic" tooltip="Italic (⌘+I)">
-              I
-            </MarkToolbarButton>
-            <MarkToolbarButton nodeType="underline" tooltip="Underline (⌘+U)">
-              U
-            </MarkToolbarButton>
-          </FixedToolbar>
-          <EditorContainer>
-            <Editor placeholder="Type your amazing content here..." />
-          </EditorContainer>
-        </Plate>
-      </StrictMode>
+      <Plate editor={editor}>
+        <FixedToolbar className="justify-start rounded-t-lg">
+          <ToolbarButton onClick={() => editor.tf.h1.toggle()}>H1</ToolbarButton>
+          <ToolbarButton onClick={() => editor.tf.h2.toggle()}>H2</ToolbarButton>
+          <ToolbarButton onClick={() => editor.tf.h3.toggle()}>H3</ToolbarButton>
+          <ToolbarButton onClick={() => editor.tf.blockquote.toggle()}>Quote</ToolbarButton>
+
+          <MarkToolbarButton nodeType="bold" tooltip="Bold (⌘+B)">
+            B
+          </MarkToolbarButton>
+          <MarkToolbarButton nodeType="italic" tooltip="Italic (⌘+I)">
+            I
+          </MarkToolbarButton>
+          <MarkToolbarButton nodeType="underline" tooltip="Underline (⌘+U)">
+            U
+          </MarkToolbarButton>
+        </FixedToolbar>
+        <EditorContainer>
+          <Editor placeholder="Type your amazing content here..." />
+        </EditorContainer>
+      </Plate>
     </div>
   );
 }
