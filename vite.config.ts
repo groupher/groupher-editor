3ceb16f2788dc00b2c3c17936a8e2109a082b78e
mydearxym <mydearxym@qq.com>
2025-09-18 16:41:53 +0800

    chore: npm publish wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,40 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// vite.config.js
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.tsx'), // targeting export file
+      name: 'RichEditor',
+      formats: ['es', 'umd'], // two export format
+      fileName: (format) => `rich-editor.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
+        // disable hash name
+        entryFileNames: `rich-editor.[format].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    },
+    // warning size
+    chunkSizeWarningLimit: 2000
+  }
 })
-
