65b3779bd81b4439740423933e3a2941684a0b43
mydearxym <mydearxym@qq.com>
2026-06-11 11:10:26 +0800

    chore(fe): upgrade platejs to v53 migration

--- src/components/editor/plugins/list-kit.tsx
+++ src/components/editor/plugins/list-kit.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { BulletedListRules, OrderedListRules, TaskListRules } from '@platejs/list';
 import { ListPlugin } from '@platejs/list/react';
 import { KEYS } from 'platejs';
 
@@ -7,6 +8,14 @@ import { BlockList } from '@/components/ui/block-list';
 
 export const ListKit = [
   ListPlugin.configure({
+    inputRules: [
+      BulletedListRules.markdown({ variant: '-' }),
+      BulletedListRules.markdown({ variant: '*' }),
+      OrderedListRules.markdown({ variant: '.' }),
+      OrderedListRules.markdown({ variant: ')' }),
+      TaskListRules.markdown(),
+      TaskListRules.markdown({ checked: true }),
+    ],
     inject: {
       targetPlugins: [...KEYS.heading, KEYS.p, KEYS.blockquote, KEYS.toggle],
     },
